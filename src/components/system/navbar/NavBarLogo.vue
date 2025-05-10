a640b230dfd337e38a53b7e6b05a56c467b90047
SandroGreenn <sandro@greenn.com.br>
2025-05-10 14:21:02 -0300

    ajustes navbar

---
+++ src/components/system/navbar/NavBarLogo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import logo from '@/assets/logo.png'
+</script>
+
+<template>
+  <v-toolbar-title class="pl-2">
+    <div class="d-flex justify-self-start align-center">
+      <v-img :src="logo" alt="Logo Soft Clinic" contain height="50" width="50" class="mr-2" />
+      <div>Soft Clinic</div>
+    </div>
+  </v-toolbar-title>
+</template>
--- src/layouts/LayoutSystem.vue
+++ src/layouts/LayoutSystem.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import logo from '@/assets/logo.png'
+import AvatarMenu from '@/components/system/navbar/AvatarMenu.vue'
+import NavBarLogo from '@/components/system/navbar/NavBarLogo.vue'
 import SideBarMenu from '@/components/system/sidebar/SideBarMenu.vue'
 import { ref, computed, onMounted } from 'vue'
 
@@ -28,28 +29,6 @@ onMounted(() => {
   updateIsMobile()
   window.addEventListener('resize', updateIsMobile)
 })
-
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-// Simulando dados do usuário logado
-const usuario = {
-  nome: 'Sandro Souza'
-}
-
-const nomeUsuario = 'Sandro Souza'
-const emailUsuario = 'sandro.souza@email.com'
-
-const siglaUsuario = computed(() => {
-  const nomes = usuario.nome.trim().split(' ')
-  if (nomes.length === 1) return nomes[0][0].toUpperCase()
-  return `${nomes[0][0]}${nomes[nomes.length - 1][0]}`.toUpperCase()
-})
-
-function irPara(path: string) {
-  router.push(path)
-}
 </script>
 
 <template>
@@ -60,54 +39,11 @@ function irPara(path: string) {
         @click="toggleAppBarIcon"
       />
 
-      <v-toolbar-title class="pl-2">
-        <div class="d-flex justify-self-start align-center">
-          <v-img :src="logo" alt="Logo Soft Clinic" contain height="50" width="50" class="mr-2" />
-          <div>Soft Clinic</div>
-        </div>
-      </v-toolbar-title>
+      <NavBarLogo />
+
       <v-spacer />
-      <v-menu offset-y>
-        <template #activator="{ props }">
-          <v-btn
-            v-bind="props"
-            class="ml-2 elevation-4 bg-white"
-            variant="text"
-            icon
-          >
-            <v-avatar size="36">
-              <span class="text-primary text-subtitle-2">{{ siglaUsuario }}</span>
-            </v-avatar>
-          </v-btn>
-        </template>
 
-        <v-list density="compact">
-          <v-list-item class="mb-1">
-            <template #prepend>
-              <v-avatar color="primary" size="40">
-                <span class="text-white text-subtitle-2">{{ siglaUsuario }}</span>
-              </v-avatar>
-            </template>
-            <v-list-item-title class="text-body-2 font-weight-medium">{{ nomeUsuario }}</v-list-item-title>
-            <v-list-item-subtitle class="text-caption text-medium-emphasis">{{ emailUsuario }}</v-list-item-subtitle>
-          </v-list-item>
-
-          <v-divider class="my-2" />
-
-          <v-list-item @click="irPara('/system/minha-conta')">
-            <v-list-item-title>Minha conta</v-list-item-title>
-          </v-list-item>
-          <v-list-item @click="irPara('/system/configuracoes')">
-            <v-list-item-title>Configurações</v-list-item-title>
-          </v-list-item>
-          <v-list-item link>
-            <template #append>
-              <v-icon color="error" class="mr-2">mdi-logout</v-icon>
-            </template>
-            <v-list-item-title class="text-error">Logout</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <AvatarMenu />
     </v-app-bar>
 
     <SideBarMenu
