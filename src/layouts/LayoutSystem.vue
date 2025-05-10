c4ff43e860c8c103111d0b6fe04c509404022bd6
SandroGreenn <sandro@greenn.com.br>
2025-05-10 14:15:00 -0300

    dropdown navbar

--- src/layouts/LayoutSystem.vue
+++ src/layouts/LayoutSystem.vue
@@ -28,6 +28,28 @@ onMounted(() => {
   updateIsMobile()
   window.addEventListener('resize', updateIsMobile)
 })
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+// Simulando dados do usuário logado
+const usuario = {
+  nome: 'Sandro Souza'
+}
+
+const nomeUsuario = 'Sandro Souza'
+const emailUsuario = 'sandro.souza@email.com'
+
+const siglaUsuario = computed(() => {
+  const nomes = usuario.nome.trim().split(' ')
+  if (nomes.length === 1) return nomes[0][0].toUpperCase()
+  return `${nomes[0][0]}${nomes[nomes.length - 1][0]}`.toUpperCase()
+})
+
+function irPara(path: string) {
+  router.push(path)
+}
 </script>
 
 <template>
@@ -44,6 +66,48 @@ onMounted(() => {
           <div>Soft Clinic</div>
         </div>
       </v-toolbar-title>
+      <v-spacer />
+      <v-menu offset-y>
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            class="ml-2 elevation-4 bg-white"
+            variant="text"
+            icon
+          >
+            <v-avatar size="36">
+              <span class="text-primary text-subtitle-2">{{ siglaUsuario }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-list density="compact">
+          <v-list-item class="mb-1">
+            <template #prepend>
+              <v-avatar color="primary" size="40">
+                <span class="text-white text-subtitle-2">{{ siglaUsuario }}</span>
+              </v-avatar>
+            </template>
+            <v-list-item-title class="text-body-2 font-weight-medium">{{ nomeUsuario }}</v-list-item-title>
+            <v-list-item-subtitle class="text-caption text-medium-emphasis">{{ emailUsuario }}</v-list-item-subtitle>
+          </v-list-item>
+
+          <v-divider class="my-2" />
+
+          <v-list-item @click="irPara('/system/minha-conta')">
+            <v-list-item-title>Minha conta</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="irPara('/system/configuracoes')">
+            <v-list-item-title>Configurações</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <template #append>
+              <v-icon color="error" class="mr-2">mdi-logout</v-icon>
+            </template>
+            <v-list-item-title class="text-error">Logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <SideBarMenu
