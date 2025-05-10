a9101133ff30647c897bc2f0f63c3a2f060f1f9a
SandroGreenn <sandro@greenn.com.br>
2025-05-10 09:04:56 -0300

    menu lateral ok

--- src/components/system/SideBarMenu.vue
+++ src/components/system/SideBarMenu.vue
@@ -1,11 +1,55 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const props = defineProps<{
+  isMobile: boolean
+  modelValue: boolean
+  rail: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
+
+const drawer = computed({
+  get: () => props.modelValue,
+  set: (val: boolean) => emit('update:modelValue', val),
+})
+
+const rail = ref(false)
+
+const menuItems = [
+  { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/system/dashboard' },
+  { title: 'Pacientes', icon: 'mdi-account', to: '/system/pacientes' },
+  { title: 'Orçamentos', icon: 'mdi-file-document-outline', to: '/system/orcamentos' },
+  { title: 'Atendimentos', icon: 'mdi-stethoscope', to: '/system/atendimentos' },
+  { title: 'Sair', icon: 'mdi-logout', to: '/logout' }
+]
+</script>
+
 <template>
-  <v-navigation-drawer app permanent color="grey-lighten-4">
-    <v-list nav>
-      <v-list-item title="Dashboard" to="/dashboard" prepend-icon="mdi-view-dashboard" />
-      <v-list-item title="Pacientes" to="/pacientes" prepend-icon="mdi-account" />
-      <v-list-item title="Orçamentos" to="/orcamentos" prepend-icon="mdi-file-document" />
-      <v-list-item title="Atendimentos" to="/atendimentos" prepend-icon="mdi-stethoscope" />
-      <v-list-item title="Sair" to="/logout" prepend-icon="mdi-logout" />
+  <v-navigation-drawer
+    v-model="drawer"
+    :permanent="!isMobile"
+    :temporary="isMobile"
+    :app="isMobile"
+    :rail="!isMobile ? rail : false"
+    width="220"
+  >
+    <v-list nav dense >
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.to"
+        link
+      >
+        <div class="d-flex align-center">
+          <v-list-item-icon class="mr-2">
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </div>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
--- src/layouts/LayoutSystem.vue
+++ src/layouts/LayoutSystem.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
 import logo from '@/assets/logo.png'
-import { ref } from 'vue'
+import SideBarMenu from '@/components/system/SideBarMenu.vue'
+import { ref, computed, onMounted } from 'vue'
 
+const drawer = ref(true)
 const rail = ref(false)
+const isMobile = ref(false)
 
-const menuItems = [
-  { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/system/dashboard' },
-  { title: 'Pacientes', icon: 'mdi-account', to: '/system/pacientes' },
-  { title: 'Orçamentos', icon: 'mdi-file-document-outline', to: '/system/orcamentos' },
-  { title: 'Atendimentos', icon: 'mdi-stethoscope', to: '/system/atendimentos' },
-  { title: 'Sair', icon: 'mdi-logout', to: '/logout' }
-]
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth < 768
+  if (isMobile.value) drawer.value = false
+}
+
+const appBarIcon = computed(() => {
+  return isMobile.value ? 'mdi-menu' : (rail.value ? 'mdi-chevron-right-box-outline': 'mdi-chevron-left-box-outline')
+})
+
+function toggleAppBarIcon() {
+  if(isMobile.value) {
+    drawer.value = !drawer.value
+  } else {
+    rail.value = !rail.value
+  }
+}
+
+onMounted(() => {
+  updateIsMobile()
+  window.addEventListener('resize', updateIsMobile)
+})
 </script>
 
 <template>
   <v-app>
-    <!-- Barra superior -->
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon
-        :icon="rail ? 'mdi-chevron-right-box-outline': 'mdi-chevron-left-box-outline'"
-        @click="rail = !rail"
+        :icon="appBarIcon"
+        @click="toggleAppBarIcon"
       />
 
       <v-toolbar-title class="pl-2">
@@ -30,30 +46,12 @@ const menuItems = [
       </v-toolbar-title>
     </v-app-bar>
 
-    <!-- Menu lateral -->
-    <v-navigation-drawer
+    <SideBarMenu
+      v-model="drawer"
       :rail="rail"
-      permanent
-      width="220"
-    >
-      <v-list nav dense >
-        <v-list-item
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
-          <div class="d-flex align-center">
-            <v-list-item-icon class="mr-2">
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </div>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+      :is-mobile="isMobile"
+    />
 
-    <!-- Conteúdo principal -->
     <v-main>
       <v-container fluid class="pa-4">
         <router-view />
