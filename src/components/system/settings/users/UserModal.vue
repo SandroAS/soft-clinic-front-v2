19bc008f4361a720590a6316e0637b744b6976e6
SandroGreenn <sandro@greenn.com.br>
2025-06-29 15:22:44 -0300

    ajuste identacao

--- src/components/system/settings/users/UserModal.vue
+++ src/components/system/settings/users/UserModal.vue
@@ -82,54 +82,110 @@ async function onSubmit(formValues: Record<string, any>) {
         </v-card-title>
         <v-card-text>
           <Field name="name" rules="required|min:3|alpha_spaces" v-slot="{ field, errorMessage }">
-            <v-text-field v-bind="field" label="Nome" prepend-inner-icon="mdi-account" variant="solo-filled"
-              density="compact" :persistent-placeholder="!!props.selectedAccountUser?.name" :error="!!errorMessage"
-              :error-messages="errorMessage" class="mb-3" />
+            <v-text-field
+              v-bind="field"
+              label="Nome"
+              prepend-inner-icon="mdi-account"
+              variant="solo-filled"
+              density="compact"
+              :persistent-placeholder="!!props.selectedAccountUser?.name"
+              :error="!!errorMessage"
+              :error-messages="errorMessage" class="mb-3"
+            />
           </Field>
 
           <Field name="email" rules="required|email" v-slot="{ field, errorMessage }">
-            <v-text-field v-bind="field" label="E-mail" prepend-inner-icon="mdi-email" variant="solo-filled"
-              density="compact" :persistent-placeholder="!!props.selectedAccountUser?.email" :error="!!errorMessage"
-              :error-messages="errorMessage" class="mb-3" />
+            <v-text-field
+              v-bind="field"
+              label="E-mail"
+              prepend-inner-icon="mdi-email"
+              variant="solo-filled"
+              density="compact"
+              :persistent-placeholder="!!props.selectedAccountUser?.email"
+              :error="!!errorMessage"
+              :error-messages="errorMessage" class="mb-3"
+            />
           </Field>
 
           <Field name="cellphone" rules="required|min:15|max:16" v-slot="{ field, errorMessage }">
-            <v-text-field v-bind="field" label="Telefone" v-mask="['(##) #####-####', '(##) ####-####']"
-              prepend-inner-icon="mdi-phone" variant="solo-filled" density="compact"
-              :persistent-placeholder="!!props.selectedAccountUser?.cellphone" :error="!!errorMessage"
-              :error-messages="errorMessage" class="mb-3" />
+            <v-text-field
+              v-bind="field"
+              label="Telefone"
+              v-mask="['(##) #####-####', '(##) ####-####']"
+              prepend-inner-icon="mdi-phone"
+              variant="solo-filled"
+              density="compact"
+              :persistent-placeholder="!!props.selectedAccountUser?.cellphone"
+              :error="!!errorMessage"
+              :error-messages="errorMessage"
+              class="mb-3"
+            />
           </Field>
 
           <Field name="cpf" rules="required|cpf" v-slot="{ field, errorMessage }">
-            <v-text-field v-bind="field" label="CPF" v-mask="'###.###.###-##'"
-              prepend-inner-icon="mdi-card-account-details" variant="solo-filled" density="compact"
-              :persistent-placeholder="!!props.selectedAccountUser?.cpf" :error="!!errorMessage"
-              :error-messages="errorMessage" class="mb-3" />
+            <v-text-field
+              v-bind="field"
+              label="CPF"
+              v-mask="'###.###.###-##'"
+              prepend-inner-icon="mdi-card-account-details"
+              variant="solo-filled"
+              density="compact"
+              :persistent-placeholder="!!props.selectedAccountUser?.cpf"
+              :error="!!errorMessage"
+              :error-messages="errorMessage"
+              class="mb-3"
+            />
           </Field>
 
-          <Field name="password" :rules="!props.selectedAccountUser?.name ? 'required|min:6' : ''"
-            v-slot="{ field, errorMessage }">
-            <v-text-field v-bind="field" label="Senha" :type="showPassword ? 'text' : 'password'"
-              prepend-inner-icon="mdi-lock" variant="solo-filled" density="compact" :error="!!errorMessage"
-              :error-messages="errorMessage" :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-              @click:append-inner="showPassword = !showPassword" class="mb-1" />
+          <Field name="password" :rules="!props.selectedAccountUser?.name ? 'required|min:6' : ''" v-slot="{ field, errorMessage }">
+            <v-text-field
+              v-bind="field"
+              label="Senha"
+              :type="showPassword ? 'text' : 'password'"
+              prepend-inner-icon="mdi-lock"
+              variant="solo-filled"
+              density="compact" :error="!!errorMessage"
+              :error-messages="errorMessage"
+              :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              @click:append-inner="showPassword = !showPassword"
+              class="mb-1"
+            />
           </Field>
           <div class="text-caption text-red mb-3">Nível de segurança da senha: <strong>Baixo</strong></div>
 
           <Field name="confirmPassword" :rules="!props.selectedAccountUser?.name ? 'required|confirmed:@password' : ''"
             v-slot="{ field, errorMessage }">
-            <v-text-field v-bind="field" label="Confirmar senha" :type="showConfirmPassword ? 'text' : 'password'"
-              prepend-inner-icon="mdi-lock" variant="solo-filled" density="compact" :error="!!errorMessage"
-              :error-messages="errorMessage" :append-inner-icon="showConfirmPassword ? 'mdi-eye-off' : 'mdi-eye'"
-              @click:append-inner="showConfirmPassword = !showConfirmPassword" class="mb-3" />
+            <v-text-field
+              v-bind="field"
+              label="Confirmar senha"
+              :type="showConfirmPassword ? 'text' : 'password'"
+              prepend-inner-icon="mdi-lock"
+              variant="solo-filled"
+              density="compact"
+              :error="!!errorMessage"
+              :error-messages="errorMessage"
+              :append-inner-icon="showConfirmPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              @click:append-inner="showConfirmPassword = !showConfirmPassword"
+              class="mb-3"
+            />
           </Field>
           <Field name="role" rules="required" v-slot="{ field, errorMessage }">
-            <v-select v-bind="field" label="Tipo" :items="userTypes" item-value="value" item-title="title"
-              item-props="disabled" :return-object="false" variant="solo-filled" density="compact"
-              persistent-placeholder :error="!!errorMessage" :error-messages="errorMessage">
+            <v-select
+              v-bind="field"
+              label="Tipo"
+              :items="userTypes"
+              item-value="value"
+              item-title="title"
+              item-props="disabled"
+              :return-object="false"
+              variant="solo-filled"
+              density="compact"
+              persistent-placeholder
+              :error="!!errorMessage"
+              :error-messages="errorMessage"
+            >
               <template v-slot:item="{ item, props: itemProps }">
-                <v-list-item v-bind="itemProps" :title="item.title" :subtitle="item.raw.subtitle"
-                  :disabled="item.raw.disabled"></v-list-item>
+                <v-list-item v-bind="itemProps" :title="item.title" :subtitle="item.raw.subtitle" :disabled="item.raw.disabled" />
               </template>
             </v-select>
           </Field>
